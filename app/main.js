1eb47d3277339c58eaa82bf6d145d0c39ffe4116
christina <158211764+christinac09@users.noreply.github.com>
2024-11-15 13:59:38 -0500

    find an api

--- app/main.js
+++ app/main.js
@@ -1,1 +1,22 @@
 import "./style.css";
+
+async function getData() {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2");
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data.data);
+      data.data.forEach((agent) =>
+        document
+          .querySelector("div")
+          .insertAdjacentHTML("afterbegin", `<h1>${agent.name}</h1>`)
+      );
+    }
+  } catch (error) {
+    alert("hey i could not find that agent");
+  }
+}
+
+getData();
