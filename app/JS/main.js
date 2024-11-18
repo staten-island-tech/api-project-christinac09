023ed6f30a85d8fcac87f877a4129e3f22032e33
christina <christinac272@nycstudents.net>
2024-11-17 23:09:16 -0500

    got cards w name and pic in container, how to style w tailwind?

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,5 +1,9 @@
 import "../CSS/style.css";
 
+const DOMSelectors = {
+  container: document.querySelector(".container")
+}
+
 async function getData() {
   try {
     const response = await fetch("https://genshin.jmp.blue/characters");
@@ -9,14 +13,15 @@ async function getData() {
       const data = await response.json();
       console.log(data);
       data.forEach((agent) =>
-        document
-          .querySelector("div")
-          .insertAdjacentHTML("beforeend", `<h1>${agent}</h1>`)
+        DOMSelectors.container.insertAdjacentHTML("beforeend", 
+            `<div class="card">
+              <h2>${agent}</h2>
+              <img src="https://genshin.jmp.blue/characters/${agent}/icon" alt="icon of ${agent}" class="card-img">
+            </div>`)
       );
     }
   } catch (error) {
     alert("hey I could not find that agent");
   }
 }
-
 getData();
