dd4ece98e3028dfa1c41302b13eef46be7c3d59f
christina <158211764+christinac09@users.noreply.github.com>
2024-11-18 14:01:25 -0500

    dSJFASDJFAS;LFJAKS HOW TO USE TAILWIND

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,8 +1,8 @@
 import "../CSS/style.css";
 
 const DOMSelectors = {
-  container: document.querySelector(".container")
-}
+  container: document.getElementById("cards-container"),
+};
 
 async function getData() {
   try {
@@ -13,11 +13,13 @@ async function getData() {
       const data = await response.json();
       console.log(data);
       data.forEach((agent) =>
-        DOMSelectors.container.insertAdjacentHTML("beforeend", 
-            `<div class="card">
+        DOMSelectors.container.insertAdjacentHTML(
+          "beforeend",
+          `<div class="w-1/6 m-8 m-auto">
               <h2>${agent}</h2>
               <img src="https://genshin.jmp.blue/characters/${agent}/icon" alt="icon of ${agent}" class="card-img">
-            </div>`)
+            </div>`
+        )
       );
     }
   } catch (error) {
