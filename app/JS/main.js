2d626606917a07f3982654029827d3f0ee733916
christina <158211764+christinac09@users.noreply.github.com>
2024-11-25 13:59:36 -0500

    afdsaf

--- app/JS/main.js
+++ app/JS/main.js
@@ -30,16 +30,11 @@ async function getData() {
 async function displayIndividualData(character, image) {
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
-    /* `<div class="w-1/6 my-8 mx-8 border-2 border-white">
-      <h2 class="text-center">${character.name}</h2>
-      <h3 class="text-center">${character.title}</h3>
-      <img src="${image}" alt="icon of ${character.name}" class="card-img">
-    </div>` */
     `<div class="card bg-base-100 w-96 shadow-xl" id="${character.id}">
       <figure>
         <img
           src="${image}"
-          alt="icon of ${character.name}" />
+          alt="icon of ${character.name}" class="card-image"/>
       </figure>
       <div class="card-body">
         <h2 class="card-title">${character.name}</h2>
